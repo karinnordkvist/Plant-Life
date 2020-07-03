e27845e6424b17a69c44d06c5d98630694c50c11
Karin Nordkvist <kontakt@karinnordkvist.se>
2020-07-03 15:22:27 +0200

    Edited menu function and design

--- menu.js
+++ menu.js
@@ -1,16 +1,20 @@
 const toggleTag = document.querySelector("a.toggle-nav")
 const mainTag = document.querySelector("main")
+const navTag = document.querySelector("nav")
 
 // When I click the toggleTag, toggle a class of open on the mainTag
 // and if its open, make the toggleTag say closed, 
 // and if its closed, make the toggleTag say Menu again
 
 toggleTag.addEventListener ("click", function(){
-    mainTag.classList.toggle("open")
+    navTag.classList.toggle("open")
 
-    if (mainTag.classList.contains("open")) {
+    //Changing appearance of the toggle button
+    if (navTag .classList.contains("open")) {
         toggleTag.innerHTML = '<img src="assets/close.svg"> Close'
+        toggleTag.style.color = "#ffffff"
     } else {
         toggleTag.innerHTML = '<img src="assets/menu.svg"> Menu'
+        toggleTag.style.color = "#2a2a2a"
     }
 })
